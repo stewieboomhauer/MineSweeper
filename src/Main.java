e037479dade84dce21a52ef722410b0e1239247d
stewieboomhauer <a.s.pushkina@bk.ru>
2019-04-19 15:11:27 +0200

    Creating constructor of the table

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Please, write dimensions!");
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int m = sc.nextInt();
+		double p = sc.nextDouble();
+
+		Table t = new Table(n, m, p);
+
+    }
+}
+
